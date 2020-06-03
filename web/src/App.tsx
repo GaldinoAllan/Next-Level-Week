85b0a0720541c89f5e508739d1dcb66d32a5bf58
Allan <allan.g.galdino@outlook.com>
2020-06-03 18:16:12 -0300

    day 3 til integrated map

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Routes from './routes';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-      </header>
-    </div>
+    <>
+      <Routes />
+    </>
   );
-}
+};
 
 export default App;
